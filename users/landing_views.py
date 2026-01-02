4fb1eda9f28dff078f9ec2ee768cf52a2ac757a2
BugBounty Arsenal <bugbounty@arsenal.dev>
2026-01-02 14:48:31 +0200

    Add login and signup page routes

--- users/landing_views.py
+++ users/landing_views.py
@@ -10,6 +10,18 @@ def landing_page(request):
     return render(request, 'landing.html')
 
 
+@require_http_methods(["GET"])
+def login_page(request):
+    """Login page view - renders login form"""
+    return render(request, 'login.html')
+
+
+@require_http_methods(["GET"])
+def signup_page(request):
+    """Signup page view - renders signup form"""
+    return render(request, 'signup.html')
+
+
 @login_required
 @require_http_methods(["GET"])
 def dashboard_page(request):
