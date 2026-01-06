a869fa6e346aee272fdc83f87ecfce4a66b11d0b
BugBounty Arsenal <bugbounty@arsenal.dev>
2026-01-06 18:27:02 +0200

    Downgrade Django to 5.1.4 for Python 3.11 compatibility and add passenger_wsgi.py for cPanel deployment

---
+++ passenger_wsgi.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+# Add virtualenv site-packages to path
+INTERP = '/home/bugbount/virtualenv/Bugbounty-backend/3.11/bin/python3'
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
+
+# Add your project directory to the sys.path
+project_home = '/home/bugbount/Bugbounty-backend'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# Set environment variables
+os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings'
+
+# Import Django WSGI application
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
